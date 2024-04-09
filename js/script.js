692a458bcb919939d4acae1ea70eee0f8360d5ce
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-04-09 20:35:10 +0000

    added and modified files and deleted pictures

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
+// Created by: Logan Connors
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+// Function to calculate the volume of a right triangular pyramid
+function calculateVolume() {
+    const sideA = parseFloat(document.getElementById('sideA').value);
+    const sideB = parseFloat(document.getElementById('sideB').value);
+    const height = parseFloat(document.getElementById('height').value);
+
+    const volume = (1/2) * sideA * height * sideB;
+
+    document.getElementById('result').innerHTML = `The volume of the right triangular pyramid is: ${volume.toFixed(3)}`;
+}
